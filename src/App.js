1c2314e924b7582947f8fbb4794571aa24dc8a94
Oscar <oscarsantana2014@gmail.com>
2021-12-06 16:08:25 -0600

    Test to display images from URL to local server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from "./components/MovieList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [movies, setMovies] = useState([
+        {
+        "Title": "Starwars: Goretech",
+        "Year": "2018",
+        "imdbID": "tt9336300",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNTI5OTBhMGYtNTZlNS00MjMzLTk5NTEtZDZkODM5YjYzYmE5XkEyXkFqcGdeQXVyMzU0OTU0MzY@._V1_SX300.jpg"
+    },
+        {
+            "Title": "MeUndies x StarWars: The Dark Side",
+            "Year": "2018",
+            "imdbID": "tt9414858",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMzg3ZDcwNDQtOTVlMC00ZjE0LThiYjktY2U2YTVjODU5MWY5XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "MeUndies x StarWars: The Force",
+            "Year": "2018",
+            "imdbID": "tt9414918",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZTdjYjM1YzgtYmViMS00ODFjLTgyM2EtMjRkNTFiNDEyMTU2XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
+        },])
+    return (
+        <div className="container-fluid movie-app">
+            <div className="row">
+              <MovieList
+              movies={movies}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
