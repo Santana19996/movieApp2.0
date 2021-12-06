6a12538d98f78b2ff76216dbafcaf9440e959e92
Oscar <oscarsantana2014@gmail.com>
2021-12-06 16:51:16 -0600

    Movie List heading and search box

--- src/App.js
+++ src/App.js
@@ -2,33 +2,20 @@ import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+
 
 function App() {
 
-    const [movies, setMovies] = useState([
-        {
-        "Title": "Starwars: Goretech",
-        "Year": "2018",
-        "imdbID": "tt9336300",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNTI5OTBhMGYtNTZlNS00MjMzLTk5NTEtZDZkODM5YjYzYmE5XkEyXkFqcGdeQXVyMzU0OTU0MzY@._V1_SX300.jpg"
-    },
-        {
-            "Title": "MeUndies x StarWars: The Dark Side",
-            "Year": "2018",
-            "imdbID": "tt9414858",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMzg3ZDcwNDQtOTVlMC00ZjE0LThiYjktY2U2YTVjODU5MWY5XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "MeUndies x StarWars: The Force",
-            "Year": "2018",
-            "imdbID": "tt9414918",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BZTdjYjM1YzgtYmViMS00ODFjLTgyM2EtMjRkNTFiNDEyMTU2XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
-        },])
+    const [movies, setMovies] = useState([])
+    const [searchValue,setSearchValue] = useState('')
     return (
         <div className="container-fluid movie-app">
+            <div className="row d-flex align-items-center mt-4 mb-4">
+                <MovieListHeading heading="Movies"/>
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+            </div>
             <div className="row">
               <MovieList
               movies={movies}/>
