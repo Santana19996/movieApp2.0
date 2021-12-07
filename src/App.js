e53d3d4731779474ef27c1fbcd4f378568facda0
Oscar <oscarsantana2014@gmail.com>
2021-12-06 18:33:49 -0600

    Add favorite option

--- src/App.js
+++ src/App.js
@@ -1,40 +1,73 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MovieList from "./components/MovieList";
-import MovieListHeading from "./components/MovieListHeading";
-import SearchBox from "./components/SearchBox";
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddFavorites';
 
 
-function App() {
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [favorites, setFavorites] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
-    const [movies, setMovies] = useState([])
-    const [searchValue,setSearchValue] = useState('')
-    const getMovieRequest = async (searchValue) =>{
-        const url= `http://www.omdbapi.com/?s=${searchValue}&apikey=ef7664dc`
+    const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=ef7664dc`;
 
         const response = await fetch(url);
         const responseJson = await response.json();
-        if (responseJson.Search){
-            setMovies(responseJson.Search)
+
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);
         }
+    };
+
+    useEffect(() => {
+        getMovieRequest(searchValue);
+    }, [searchValue]);
+
+    useEffect(() => {
+        const movieFavorites = JSON.parse(
+            localStorage.getItem('react-movie-app-favorites')
+        );
+
+        if (movieFavorites) {
+            setFavorites(movieFavorites);
         }
-        useEffect(() => {
-            getMovieRequest(searchValue)
-        }, [searchValue])
+    }, []);
+
+    const saveToLocalStorage = (items) => {
+        localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
+    };
+
+    const addFavoriteMovie = (movie) => {
+        const newFavoriteList = [...favorites, movie];
+        setFavorites(newFavoriteList);
+        saveToLocalStorage(newFavoriteList);
+    };
+
+
+
     return (
-        <div className="container-fluid movie-app">
-            <div className="row d-flex align-items-center mt-4 mb-4">
-                <MovieListHeading heading="Movies"/>
-                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+        <div className='container-fluid movie-app'>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Movies' />
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
             </div>
-            <div className="row">
-              <MovieList
-              movies={movies}/>
+            <div className='row'>
+                <MovieList
+                    movies={movies}
+                    handleFavoritesClick={addFavoriteMovie}
+                    favoriteComponent={AddFavorites}
+                />
             </div>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Favorites' />
+            </div>
+
         </div>
     );
-}
-
+};
 
 export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,20 @@
 const MovieList = (props) => {
+    const FavoriteComponent = props.favoriteComponent;
     return (
         <>
-            {props.movies.map((movie,index) =>(
-<div className="image-container d-flex justify-content-start m3">
-    <img src={movie.Poster} alt="movie"/>
-
-</div>
+            {props.movies.map((movie, index) => (
+                <div className="image-container d-flex justify-content-start m3">
+                    <img src={movie.Poster} alt="movie"/>
+                    <div
+                        onClilck={() => props.handleFavoritesClick(movie)}
+                        className="overlay d-flex align-items-center jusitfy-content-center"
+                    >
+                        < FavoriteComponent/>
+                    </div>
+                </div>
             ))}
-            </>
+        </>
     )
 }
 
-export default MovieList
+export default MovieList;
